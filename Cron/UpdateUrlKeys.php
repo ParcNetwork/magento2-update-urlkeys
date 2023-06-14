cf5dcdd978fd61c6d32655fbe29278200e2490db
yolomir <ltempleton@artplants.eu>
2023-06-14 14:43:26 +0200

    removed testint purpose of updateurlkeys

--- Cron/UpdateUrlKeys.php
+++ Cron/UpdateUrlKeys.php
@@ -98,8 +98,6 @@ class UpdateUrlKeys
     public function execute(): void
     {
         $settings = $this->import->run();
-        $test = [['test']];
-        $this->csvExporter->exportData($test, 'test');
 
         $storeViewSettings = $settings['storeViewsSettings'];
 
