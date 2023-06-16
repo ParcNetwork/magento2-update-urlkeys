deeae9b3afb08d77c27184f0df5f5c6fe50d5e11
yolomir <ltempleton@artplants.eu>
2023-06-16 15:01:17 +0200

    added db tables for job results

---
+++ Model/ResourceModel/JobResults/Collection.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © Louis Templeton All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Parc\UpdateUrlKeys\Model\ResourceModel\JobResults;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Parc\UpdateUrlKeys\Model\JobResults;
+
+class Collection extends AbstractCollection
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected $_idFieldName = 'jobresults_id';
+
+    /**
+     * @inheritDoc
+     */
+    protected function _construct()
+    {
+        $this->_init(
+            JobResults::class,
+            \Parc\UpdateUrlKeys\Model\ResourceModel\JobResults::class
+        );
+    }
+}
